86cf29a93b4b355c31b83bf5434237e37bf022eb
sumitdhyani <sumitdhyani1@gmail.com>
2025-02-18 03:49:35 +0530

    1st Unit test

---
+++ UnitTests/MergingEmptyFiles.cpp
@@ -0,0 +1,45 @@
+#include <cassert>
+#include <string.h>
+#include <iostream>
+#include <Solution.h>
+
+int main()
+{
+  const char* header = "Symbol, Timestamp, Price, Size, Exchange, Type";
+
+  FileReaderProvider fileReaderProvider =
+  [header](const std::string&)
+  {
+    return [](char* buff)
+    {
+      return 0;
+    };
+  };
+
+  uint32_t totalLen;
+  char outBuffer[256];
+  FileWriterProvider fileWriterProvider =
+  [header, &outBuffer, &totalLen](const std::string&)
+  {
+    return [&outBuffer, &totalLen, &header](const char* buff, uint32_t len)
+    {
+      memcpy(outBuffer, buff, len);
+      totalLen += len;
+    };
+  };
+
+  uint8_t numThreads = 2;
+  auto inputFiles = std::make_shared<std::queue<std::string>>();
+  inputFiles->push("file1.txt");
+  inputFiles->push("file2.txt");
+  entryPoint(numThreads, 
+             2,
+             inputFiles,
+             std::make_shared<std::mutex>(),
+             mergeFiles,
+             fileReaderProvider,
+             fileWriterProvider);
+  assert(0 == memcmp(outBuffer, header, strlen(header)));
+  assert(totalLen == strlen(header) + 1);
+  assert(outBuffer[totalLen-1] == '\n');
+}
